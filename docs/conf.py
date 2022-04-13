f8d85e3d49cf91c6832c68e618a1aa14a283f04c
Hillel <h@hillelwayne.com>
2022-04-13 15:28:28 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'H'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'piccolo_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
